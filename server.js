2882a30c85358df7531e42f3dd14453aa648bcc5
udaykumar-8329 <uday.lasyathota@gmail.com>
2021-10-06 13:38:05 +0530

    added script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/frontend'));
+
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/frontend/index.html'));
 });
--- src/app/services/AuthInterceptor.service.ts
+++ src/app/services/AuthInterceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -7,12 +7,13 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
-
+    let headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*',
+              'content-type': 'application/json', 'Authorization' : `Bearer ${token}` }  )
     if (!token) {
       return next.handle(req);
     }
     const newRequest = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
+      headers: headers
     });
 
     return next.handle(newRequest);
